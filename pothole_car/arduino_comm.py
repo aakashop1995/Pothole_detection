79bf600c67b18b736af5a5212668878f962fa5d8
aakashop1995 <aakashop1995@gmail.com>
2026-05-20 16:30:47 +0530

    Update arduino_comm.py

--- pothole_car/arduino_comm.py
+++ pothole_car/arduino_comm.py
@@ -4,20 +4,21 @@ import time
 arduino = None
 
 try:
-
-    arduino = serial.Serial('/dev/ttyACM0', 9600)
-
+    arduino = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
     time.sleep(2)
 
-    print("Arduino connected")
+    print("Arduino connected on /dev/ttyACM0")
 
-except:
-
-    print("Arduino not connected")
+except Exception as e:
+    print("Arduino not connected:", e)
 
 
 def send_command(command):
 
-    if arduino:
+    if arduino is not None:
 
-        arduino.write(command.encode())
+        try:
+            arduino.write((command + "\n").encode())
+
+        except Exception as e:
+            print("Send error:", e)
