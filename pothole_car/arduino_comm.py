7576affbbf4a63ba7832eb405470579b3ff5d16a
aakashop1995 <aakashop1995@gmail.com>
2026-05-20 15:06:46 +0530

    Update arduino_comm.py

--- pothole_car/arduino_comm.py
+++ pothole_car/arduino_comm.py
@@ -1,10 +1,23 @@
 import serial
 import time
 
-arduino = serial.Serial('/dev/ttyUSB0', 9600)
+arduino = None
 
-time.sleep(2)
+try:
+
+    arduino = serial.Serial('/dev/ttyUSB0', 9600)
+
+    time.sleep(2)
+
+    print("Arduino connected")
+
+except:
+
+    print("Arduino not connected")
+
 
 def send_command(command):
 
-    arduino.write(command.encode())
+    if arduino:
+
+        arduino.write(command.encode())
